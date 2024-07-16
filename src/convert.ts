c4f193dabca5f45546cd4dae7ac1dbd76862c63e
fxzer <fxzer8888@163.com>
2024-07-16 23:50:00 +0800

    feat: add file size format

--- src/convert.ts
+++ src/convert.ts
@@ -9,3 +9,14 @@ export function convertBytes(bit: number, unit: '' | 'bps' = '') {
   const converted = bit / (1 << (i * 10))
   return `${converted.toFixed(2)} ${sizes[i]}${unit}`
 }
+
+/**  格式化文件大小 */
+export function fileSizeFormat(size: number) {
+  const units = ['B', 'KB', 'MB', 'GB', 'TB']
+  let index = 0
+  while (size >= 1024 && index < units.length - 1) {
+    size /= 1024
+    index++
+  }
+  return `${size.toFixed(2)}${units[index]}`
+}
